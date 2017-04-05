ff1eaca09de2ce59a4edd984e01db77cabe0a96f
Ziad Ahmed <z.ahmed@live.com>
2017-04-05 21:22:16 +0000

    final final version :D

--- character-positioning.js
+++ character-positioning.js
@@ -2,7 +2,7 @@ function countLetters(word) {
   var object = {};
   // var indicies = [];
   // split the word into an array of characters
-  var word_split = word.split("");
+  //var word_split = word.split("");
   for (var i = 0; i < word.length; i++) {
     if (!object[word[i]]) {
       object[word[i]] = {count: 1, indices: [i]};
